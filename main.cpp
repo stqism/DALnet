e9cf5ffb34bae3f30c92331206a6da0e3c942071
Sean Qureshi <stqism@risingstormgames.com>
2013-06-04 19:21:36 -0700

    C style dynamic memory allocation!

--- main.cpp
+++ main.cpp
@@ -18,18 +18,22 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 #include <sanitize.h>
 using namespace std;
 
 int main() {
-	char file[],eval[];
+	char *file, *eval;
+	file=(char*) malloc (1);
+	eval=(char*) malloc (&file + 1);
 	if (!cin.eof()) {cin >> file;
 	} else { cout << 'Error, no data provided'; break}; //please work
 	ifstream data(file);
 	eval[0] = 0;
 	for (int i = 0;!data.get();i++) {
 		eval[i] = data.get(); count eval[i];
-					}
+				}
+	free (file);
 	for(int i;!eval[i];i++){purge(eval[i],eval[i-1});
 	}
 	
